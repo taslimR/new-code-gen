45ed5a6a25309e165af28df3d6a679c3f7beeb62
taslimR <hridoy.bubd@gmail.com>
2018-03-22 20:50:55 +0600

    first commit

---
+++ bin/commit3.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+//console.log('Hello, world!');
+var program = require('commander');
+program
+ .option('--cell-count <cellCount>', 'number of cells')
+ .option('--ratio <ratio>', 'ratio of width and height')
+ .option('--width <width>', 'width')
+ .option('--height <height>', 'height')
+ .option('--filled-cell-count <filledCellCount>', 'filled cell amount')
+.parse(process.argv);
+
+let n = program.cellCount;
+let nf = program.filledCellCount;
+
+while(true)
+{
+	let a = Math.floor(Math.random() * n);
+	const mySet = new Set();
+	
+	mySet.add(a);
+	if(nySet.size == nf)
+		break;
+}
+
+		
+		console.log(`{ for (let item of mySet) console.log(item); }`);
